98e533da1bfd4278b162a365245781b1ba33f57e
bagrounds <bagrounds@gmail.com>
2017-05-13 15:25:24 -0700

    [MINOR][ADD] empty, rightPad, and leftPad functions

--- src/index.js
+++ src/index.js
@@ -11,13 +11,58 @@
   var array = require('fun-array')
   var guarded = require('guarded')
 
-  var stringsIn = guarded(array.all(predicate.type('String')))
-  var stringsToString = stringsIn(predicate.type('String'))
+  var isString = predicate.type('String')
+
+  var stringsIn = guarded(array.all(isString))
+  var stringsToString = stringsIn(isString)
+  var stringNumberStringIn = guarded(predicate.type('(String, Number, String)'))
+  var snsToString = stringNumberStringIn(isString)
 
   /* exports */
   module.exports = {
     prepend: fn.curry(stringsToString(prepend)),
-    append: fn.curry(stringsToString(append))
+    append: fn.curry(stringsToString(append)),
+    empty: empty,
+    leftPad: fn.curry(snsToString(leftPad)),
+    rightPad: fn.curry(snsToString(rightPad))
+  }
+
+  /**
+   *
+   * @function module:fun-string.empty
+   *
+   * @return {String} the empty string
+   */
+  function empty () {
+    return ''
+  }
+
+  /**
+   *
+   * @function module:fun-string.rightPad
+   *
+   * @param {String} value - to pad with
+   * @param {Number} length - of the resulting string
+   * @param {String} s - to pad
+   *
+   * @return {String} padded with value to length
+   */
+  function rightPad (value, length, s) {
+    return array.rightPad(value, length, s.split('')).reduceRight(append, '')
+  }
+
+  /**
+   *
+   * @function module:fun-string.leftPad
+   *
+   * @param {String} value - to pad with
+   * @param {Number} length - of the resulting string
+   * @param {String} s - to pad
+   *
+   * @return {String} padded with value to length
+   */
+  function leftPad (value, length, s) {
+    return array.leftPad(value, length, s.split('')).reduce(prepend, '')
   }
 
   /**
--- test/tests.js
+++ test/tests.js
@@ -8,6 +8,11 @@
   var arrange = require('fun-arrange')
 
   var equalityTests = [
+    [[], '', 'empty'],
+    [['_', 5, 'abcdef'], 'abcdef', 'rightPad'],
+    [['_', 5, 'abc'], 'abc__', 'rightPad'],
+    [[' ', 5, 'abcdef'], 'abcdef', 'leftPad'],
+    [[' ', 5, 'abc'], '  abc', 'leftPad'],
     [['pre', 'string'], 'prestring', 'prepend'],
     [['post', 'string'], 'stringpost', 'append']
   ].map(arrange({ inputs: 0, predicate: 1, contra: 2 }))
